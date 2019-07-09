3d2524090d1e9dd8e56bd6789ee8b0498cd2149c
ailobe <aitorlopezbeltran@gmail.com>
2019-07-09 17:39:09 +0200

    Fix guard clause

--- app/decorators/decidim/proposals/endorse_proposal_decorator.rb
+++ app/decorators/decidim/proposals/endorse_proposal_decorator.rb
@@ -3,8 +3,7 @@
 Decidim::Proposals::EndorseProposal.class_eval do
   # Add guard clause for emendations: cannot endorse without user group and the same scope.
   def call
-    return broadcast(:invalid) if @proposal.emendation? && @current_group_id.nil? ||
-                                  @proposal.emendation? && @proposal.scope != @current_user.scope
+    return broadcast(:invalid) if @proposal.emendation? && (@current_group_id.nil? || @proposal.scope != @current_user.scope)
 
     endorsement = build_proposal_endorsement
     if endorsement.save
--- app/decorators/decidim/proposals/proposal_endorsements_helper_decorator.rb
+++ app/decorators/decidim/proposals/proposal_endorsements_helper_decorator.rb
@@ -4,7 +4,7 @@ Decidim::Proposals::ProposalEndorsementsHelper.class_eval do
   # Add guard clause for emendations: don't show the endorsement button if
   # the user is not a manager of a grup or has a different scope than the proposal.
   def render_endorsements_button_card_part(proposal, fully_endorsed, html_class = nil)
-    return if proposal.emendation? && manageable_user_groups.empty? || proposal.scope != current_user.scope
+    return if proposal.emendation? && (manageable_user_groups.empty? || proposal.scope != current_user.scope)
 
     endorse_translated = t("decidim.proposals.proposal_endorsements_helper.render_endorsements_button_card_part.endorse")
     html_class = "card__button button" if html_class.blank?
