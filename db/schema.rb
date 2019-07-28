271f93750b5f8892c98e69b285f917a7e738b309
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2019-07-29 00:33:56 +0200

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_110926) do
+ActiveRecord::Schema.define(version: 2019_07_25_110927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -1226,6 +1226,7 @@ ActiveRecord::Schema.define(version: 2019_07_25_110926) do
     t.string "notification_types", default: "all", null: false
     t.datetime "officialized_at"
     t.jsonb "officialized_as"
+    t.integer "decidim_scope_id"
     t.index ["confirmation_token"], name: "index_decidim_users_on_confirmation_token", unique: true
     t.index ["decidim_organization_id"], name: "index_decidim_users_on_decidim_organization_id"
     t.index ["email", "decidim_organization_id"], name: "index_decidim_users_on_email_and_decidim_organization_id", unique: true, where: "((deleted_at IS NULL) AND (managed = false) AND ((type)::text = 'Decidim::User'::text))"
