29520f6a3a2e5ccf2ed30551d0bc6d6b10400343
ailobe <aitorlopezbeltran@gmail.com>
2019-08-02 14:19:53 +0200

    Update initializer

--- config/initializers/decidim_erc_crm_authenticable.rb
+++ config/initializers/decidim_erc_crm_authenticable.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-module Decidim
-  module Erc
-    module CrmAuthenticable
-      filepath = Rails.root.join("config", "civi_crm", "local_comarcal_relationships.yml")
-      SCOPE_CODES = YAML.load_file(filepath)
-    end
-  end
+unless Rails.env.test?
+  filepath = Rails.root.join("config", "civi_crm", "local_comarcal_relationships.yml")
+  raise "Run `bundle exec rake civi_crm:init`" unless File.exist?(filepath)
+
+  Decidim::Erc::CrmAuthenticable::SCOPE_CODES = YAML.load_file(filepath)
 end
