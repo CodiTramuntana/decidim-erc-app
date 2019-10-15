58f04e948cda41b63fb2ef7f52072543f6f88a1b
ailobe <aitorlopezbeltran@gmail.com>
2019-10-15 12:40:30 +0200

    Update initializer

--- config/initializers/decidim_erc_crm_authenticable.rb
+++ config/initializers/decidim_erc_crm_authenticable.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
-unless Rails.env.test?
-  filepath = Rails.root.join("config", "civi_crm", "local_comarcal_relationships.yml")
-  Decidim::Erc::CrmAuthenticable::SCOPE_CODES = YAML.load_file(filepath) if File.exist?(filepath)
-end
+Decidim::Erc::CrmAuthenticable::CIVICRM_COMARCAL_EXCEPTIONS = [
+  "Baix Llobregat (comarcal)",
+  "Barcelones Nord (comarcal)",
+  "Maresme (comarcal)",
+  "Vallès Occidental (comarcal)",
+  "Vallès Oriental (comarcal)"
+].freeze
+filepath = Rails.root.join("config", "civi_crm", "decidim_scopes_mapping.yml")
+Decidim::Erc::CrmAuthenticable::SCOPE_CODES = YAML.load_file(filepath).freeze if File.exist?(filepath)
+Decidim::Erc::CrmAuthenticable::VALID_MBSP_STATUS_IDS = %w(1 2).freeze
+Decidim::Erc::CrmAuthenticable::VALID_MBSP_JOIN_DATE = Date.parse("21-12-2019").freeze
