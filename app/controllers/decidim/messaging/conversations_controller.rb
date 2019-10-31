6e3115d71ecd62e35a077b50d5df5ea578d95b3b
Isaac <isaac.mg@coditramuntana.com>
2019-10-31 13:08:28 +0100

    remove the conversations functionality

---
+++ app/controllers/decidim/messaging/conversations_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# This controller is overwritting the functionality of conversations for ERC.
+# Remove if they want to enable again.
+module Decidim
+  module Messaging
+    class ConversationsController < Decidim::ApplicationController
+      before_action :redirect_to_not_found
+      
+      private
+
+      def redirect_to_not_found
+        raise ActionController::RoutingError, "Not Found"
+      end
+    end
+  end
+end
