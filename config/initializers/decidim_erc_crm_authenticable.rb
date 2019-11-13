c59c712b8e64e07a3586f98b2ef5ea754e14a38b
ailobe <aitorlopezbeltran@gmail.com>
2019-11-13 14:36:02 +0100

    Update: decidim-erc-crm_authenticable gem

--- config/initializers/decidim_erc_crm_authenticable.rb
+++ config/initializers/decidim_erc_crm_authenticable.rb
@@ -10,5 +10,6 @@ Decidim::Erc::CrmAuthenticable::CIVICRM_COMARCAL_EXCEPTIONS = %w(
 ).freeze
 filepath = Rails.root.join("config", "civi_crm", "decidim_scopes_mapping.yml")
 Decidim::Erc::CrmAuthenticable::SCOPE_CODES = YAML.load_file(filepath).freeze if File.exist?(filepath)
+Decidim::Erc::CrmAuthenticable::VALID_MBSP_NAMES = %w(Militant).freeze
 Decidim::Erc::CrmAuthenticable::VALID_MBSP_STATUS_IDS = %w(1 2).freeze
 Decidim::Erc::CrmAuthenticable::VALID_MBSP_JOIN_DATE = Date.parse("11-08-2019").freeze
