09d1f0d96640b10906958c7b39895b9ea747a5ff
ailobe <aitorlopezbeltran@gmail.com>
2019-11-20 18:23:58 +0100

    Refactor thanks to review

--- app/decorators/decidim/proposals/proposal_serializer_decorator.rb
+++ app/decorators/decidim/proposals/proposal_serializer_decorator.rb
@@ -4,10 +4,10 @@
 # We are calling the original method and then tapping into it in order
 # to mutate the return value. See: https://stackoverflow.com/a/4471202
 Decidim::Proposals::ProposalSerializer.class_eval do
-  original_method = instance_method(:serialize)
+  alias_method :original_serialize, :serialize
 
-  define_method(:serialize) do
-    original_method.bind(self).call.tap do |hsh|
+  def serialize
+    original_serialize.tap do |hsh|
       hsh.merge!(nickname: proposal.creator_author.try(:nickname))
     end
   end
