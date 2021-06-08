eac6bae45c112a6172e8c7907b4e3f85bb00b787
Laura Jaime <laura.jv@coditramuntana.com>
2021-06-08 15:57:19 +0200

    Add amendments exportations dropdown with scopes

--- app/overrides/decidim/proposals/admin/proposals/add_amendment_exportations_to_proposals.rb
+++ app/overrides/decidim/proposals/admin/proposals/add_amendment_exportations_to_proposals.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-Deface::Override.new(virtual_path: "decidim/proposals/admin/proposals/_bulk-actions",
+Deface::Override.new(virtual_path: +"decidim/proposals/admin/proposals/_bulk-actions",
                      name: "add_amendment_exportations_to_proposal",
                      insert_before: "erb[loud]:contains('new_proposals_import_path')",
-                     text: "<%= link_to t('actions.export_amendments', scope: 'decidim'),
-                        Rails.application.routes.url_helpers.amendment_exports_path(params[:participatory_process_slug] || params[:assembly_slug],
-                          params[:component_id]), method: :post, class: 'button tiny button--simple' %>
-
-                      ")
+                     text: "<%= export_amendments_dropdown %>",
+                     original: '73e9d0771125e8dc2d7655f303cbbcff375c18a6')
