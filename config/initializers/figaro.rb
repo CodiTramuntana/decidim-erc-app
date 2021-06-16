fe544435cb36cfd2584b042374a74fac0c105c16
Laura Jaime <laura.jv@coditramuntana.com>
2021-06-16 10:22:58 +0200

    Remove secret_key from figaro

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 env = Rails.env
-keys = %w(SECRET_KEY_BASE)
+keys = %w()
 unless env.development? || env.test?
   keys += %w(DB_DATABASE DB_PASSWORD DB_USERNAME)
   keys += %w(MAILER_SMTP_ADDRESS MAILER_SMTP_DOMAIN MAILER_SMTP_PORT MAILER_SMTP_USER_NAME MAILER_SMTP_PASSWORD)
