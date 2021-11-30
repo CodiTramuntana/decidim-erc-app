154a3e1c736970faa33b6f0f9c675716f7f2c4aa
Laura Jaime <laura.jv@coditramuntana.com>
2021-11-30 14:27:09 +0100

    Set scope to admins

---
+++ db/migrate/20211130123421_add_scope_to_admins.rb
@@ -0,0 +1,22 @@
+class AddScopeToAdmins < ActiveRecord::Migration[5.2]
+  def change
+    admins = Decidim::User.where(admin: true)
+
+    if admins.present?
+      organization = Decidim::Organization.first
+
+      scope = Decidim::Scope.find_or_create_by!(
+        decidim_organization_id: organization.id,
+        name: { "ca": "Organització", "en": "Organization", "es": "Organization" },
+        code: "1"
+      )
+
+      admins.find_each do |admin|
+        unless admin.scope.present?
+          admin.scope = scope
+          admin.save
+        end
+      end
+    end
+  end
+end
---
+++ spec/commands/decidim/invite_user_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Decidim
+  describe InviteUser do
+    let(:organization) { create(:organization) }
+    let!(:admin) { create(:user, :confirmed, :admin, organization: organization) }
+    let!(:scope) { create(:scope, organization: organization, code: "1") }
+    let(:form) do
+      Decidim::InviteUserForm.from_params(
+        name: "Old man",
+        email: "oldman@email.com",
+        organization: organization,
+        role: "admin",
+        invited_by: admin,
+        invitation_instructions: "invite_admin"
+      )
+    end
+    let!(:command) { described_class.new(form) }
+    let(:invited_user) { User.where(organization: organization).last }
+
+    context "when a user does not exist for the given email" do
+      it "creates it with scope when user is admin" do
+        expect do
+          command.call
+        end.to change(User, :count).by(1)
+
+        expect(invited_user.email).to eq(form.email)
+        expect(Decidim::User.last.scope.code).to eq(scope.code)
+      end
+
+      it "creates it without scope when user is not admin" do
+        form.role = "user_manager"
+
+        expect do
+          command.call
+        end.to change(User, :count).by(1)
+
+        expect(invited_user.email).to eq(form.email)
+        expect(Decidim::User.last.scope).not_to eq(scope)
+      end
+
+      it "broadcasts ok and the user" do
+        expect do
+          command.call
+        end.to broadcast(:ok)
+      end
+    end
+  end
+end
