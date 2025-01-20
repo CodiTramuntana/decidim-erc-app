429053761154c45cbc5eb00fc9f915b9bacd03ba
Laura Jaime <laura.jv@coditramuntana.com>
2025-01-20 16:55:37 +0100

    Upgrade to Decidim v0.29

---
+++ app/packs/src/decidim/decidim_application.js
@@ -0,0 +1,13 @@
+// This file is compiled inside Decidim core pack. Code can be added here and will be executed
+// as part of that pack
+
+// Load images
+require.context("../../images", true)
+
+// TODO: proposals
+// document.addEventListener("DOMContentLoaded", function() {
+//   let options = $("#amendment_sectorial_commission")[0].options;
+
+//   options[1].setAttribute("disabled", "disabled");
+//   options[2].setAttribute("disabled", "disabled");
+// });
--- db/migrate/20211220115347_add_sectorial_commission_to_proposals.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Add sectorial_commission to be able to make amendments to the proposals with this field
-class AddSectorialCommissionToProposals < ActiveRecord::Migration[5.2]
-  def change
-    add_column :decidim_proposals_proposals, :sectorial_commission, :integer
-  end
-end
