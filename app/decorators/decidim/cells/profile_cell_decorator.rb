21ef051a50676b3022c10ccc7b799298c2c50df4
Laura Jaime <laura.jv@coditramuntana.com>
2025-01-27 14:19:40 +0100

    Fix decorators

--- app/decorators/decidim/cells/profile_cell_decorator.rb
+++ app/decorators/decidim/cells/profile_cell_decorator.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 # This decorator handles when to show some links on the profile sidebar.
-Decidim::ProfileCell.class_eval do
-  # Method overrided.
-  # Only admins can view the links to "Edit profile" and "Create group".
-  def own_profile?
-    current_user&.admin? && current_user == profile_holder
+module Decidim::Cells::ProfileCellDecorator
+  def self.decorate
+    Decidim::ProfileCell.class_eval do
+      # Method overrided.
+      # Only admins can view the links to "Edit profile" and "Create group".
+      def own_profile?
+        current_user&.admin? && current_user == profile_holder
+      end
+    end
   end
 end
+
+::Decidim::Cells::ProfileCellDecorator
