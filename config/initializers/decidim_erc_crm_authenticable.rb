ed389ac73b345117e7de77fd296faf9f054ad7d6
Laura Jaime <laura.jv@coditramuntana.com>
2025-01-29 11:21:46 +0100

    Generate 2025 yml with scopes

--- config/initializers/decidim_erc_crm_authenticable.rb
+++ config/initializers/decidim_erc_crm_authenticable.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 # https://github.com/CodiTramuntana/decidim-erc-crm_authenticable/blob/master/lib/decidim/erc/crm_authenticable.rb
-Decidim::Erc::CrmAuthenticable::CIVICRM_COMARCAL_EXCEPTIONS = %w(
-  9300
-  2100
-  5600
-  9200
-  9900
-).freeze
+Decidim::Erc::CrmAuthenticable::CIVICRM_COMARCAL_EXCEPTIONS = %w().freeze
 filepath = Rails.root.join("config", "civi_crm", "decidim_scopes_mapping.yml")
 Decidim::Erc::CrmAuthenticable::SCOPE_CODES = YAML.load_file(filepath).freeze if File.exist?(filepath)
 Decidim::Erc::CrmAuthenticable::VALID_MBSP_NAMES = %w(Militant).freeze
